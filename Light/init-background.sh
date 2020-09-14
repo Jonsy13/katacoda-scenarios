1ad1f5d2b0698b0c9dc12213c9845555670c72c5
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-14 10:23:51 +0530

    Katacoda.
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- Light/init-background.sh
+++ Light/init-background.sh
@@ -17,17 +17,6 @@ docker run whalebrew/pygmentize:2.6.1 &
 echo 'function ccat() { docker run -it -v "$(pwd)":/workdir -w /workdir whalebrew/pygmentize:2.6.1 $@; }' >> ~/.bashrc
 source ~/.bashrc
 
-# Setup dashboard on port 30000
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm install dash kubernetes-dashboard/kubernetes-dashboard \
---version=2.3.0 \
---namespace kube-system \
---set=image.tag=v2.0.3 \
---set=service.type=NodePort \
---set=enableInsecureLogin=true \
---set=service.nodePort=30000 \
---set=service.externalPort=80
-
 # Katacoda Cloud Provider is used when a LoadBalancer service is requested 
 # by Kubernetes, Katacoda will respond with the IP of the master. This is 
 # how Istio and other LoadBalancer based services can be deployed.
